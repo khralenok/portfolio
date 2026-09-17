87f93d635cb76b6346b785cb813cb7e641e8bf5a
Grigorii Khralenok <grigoriikhralenok@Mac.lan>
2026-09-17 17:50:15 +0700

    Astro like setup was implemented

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://khralenok.com',
+    fonts: [{
+        provider: fontProviders.google(),
+        name: "Inter",
+        cssVariable: "--font-inter",
+        styles: ["normal"],
+        weights: [400, 600, 900]
+    }]
 });
